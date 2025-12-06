bbbe382d3afda201b5cd53a2d7c3337f9eb6f03c
techabby <abbyheere@gmail.com>
2025-12-07 00:34:25 +0500

    Script

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Preload -- Loading will end after window is loaded
+const preloader = document.querySelector('[data-preload]');
+window.addEventListener("load", function () {
+    preloader.classList.add('loaded');
+    document.body.classList.add('loaded');
+});
+// Scrolled
+window.addEventListener('scroll', function () {
+    let navbar = this.document.querySelector('.navbar');
+    if (this.window.scrollY > 100) {
+        navbar.classList.add('scrolled')
+    }
+    else {
+        navbar.classList.remove('scrolled')
+    }
+})
+// Open Reservation
+function reservation() {
+    window.location.href = '/pages/reservation.html';
+}
